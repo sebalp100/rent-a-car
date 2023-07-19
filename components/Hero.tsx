fb05c5ce3d2f9a9c2412288cf15a3a9e1be95191
sebalp100 <sebalp100@gmail.com>
2023-07-19 20:31:45 -0300

    Add basic home page and tailwind styles

---
+++ components/Hero.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x"></div>
+    </div>
+  );
+}
+
+export default Hero;
