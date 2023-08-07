521e64e32d532b9497f9d2fc121a2fbf474d70d7
sebalp100 <sebalp100@gmail.com>
2023-08-06 23:32:18 -0300

    Add new section

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="discover"></div>
     </main>
   );
 }
